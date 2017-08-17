1c6a136607ebbf4d16bf9b4f9bc3af45be9dc0e6
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-17 21:44:05 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,23 +9,26 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    #binding.pry ~ it's making it here
+    erb :'figures/edit'
+  end
+
   post '/figures' do
 
     @figure = Figure.create(name: params[:figure][:name])
 
-    @figure.titles << Title.first
-    @figure.landmarks << Landmark.first
-# need to figure a way to check if titles or landmarks exist, if not create them
 
-    #if !params[:title][:name].empty?
-    #  @figure.titles << Title.create(params[:title])
-    #  binding.pry
-    #end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
 
-    #if !params[:landmark][:name].empty?
-    #  @figure.landmarks << Landmark.create(params[:landmark])
-    #end
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
 
+    redirect "/figures/#{@figure.id}"
   end
 
 
