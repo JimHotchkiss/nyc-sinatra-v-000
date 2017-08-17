4cef76373ccdfea8e78e417e28c8ad51ee1ca0c1
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-17 17:53:33 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,11 +1,17 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
   get '/figures/new' do
     erb :'/figures/new'
   end
 
   post '/figures' do
-    @figure = Figure.find_by(params[:id])
+    # a figure can have many landmarks, this will be an array
+    # a figure can have many titles, this will be an array
     @figure = Figure.create(params[:figure])
     binding.pry
   end
