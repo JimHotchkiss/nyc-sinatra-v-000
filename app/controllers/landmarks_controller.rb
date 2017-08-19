ac2b9eaecf6ac2c0180674de1c94707e6b304da5
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-19 12:13:39 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,7 +1,17 @@
 class LandmarksController < ApplicationController
 
   get '/landmarks' do
+    @landmarks = Landmark.all
     erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+
   end
 
 end
